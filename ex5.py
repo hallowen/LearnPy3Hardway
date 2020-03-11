c502796fcd682113aae9e1501d568a951078fc72
hallowen <sandeepsuresh2006@gmail.com>
2020-03-12 00:30:31 +0530

    Format string in Python

---
+++ ex5.py
@@ -0,0 +1,21 @@
+#Format string in Python
+
+my_name = 'Sandeep Suresh'
+my_age = 32
+my_height = 178
+my_weight = 88
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm's tall.")
+print(f"He's {my_weight} kilograms in weight.")
+print("Actually that is not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth is usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
+
+
